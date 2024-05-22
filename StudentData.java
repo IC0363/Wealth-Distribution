4ccd5138571201bc6c25f1e4b121315f53a7beeb
IC0363 <ichoi0221@gmail.com>
2024-05-22 13:26:11 -0400

    Update StudentData.java

--- StudentData.java
+++ StudentData.java
@@ -19,22 +19,56 @@ public class StudentData {
       
       
       /* populate unformattedData array with data from the table */
-      while(scan.hasNextLine() == true){
-        scan.nextLine();
+      while(scan.hasNextLine()){
+        String s = "";
+        s=scan.nextLine();
+        s.replaceAll("/","");
+        unformattedData.add(s);
     }
+    scan.close();
       /* use String method replaceAll(String replace, String replacement) to get rid double quotes */
-      
       /* parse through the first row of unformattedData to determine the number of columns */
+      String str  = unformattedData.get(0);
+      int count = 0;
+      int i = str.indexOf(",");
+      while(i !=-1){
+        count++;
+        str = str.substring(str.indexOf(",")+1);
+        i = str.indexOf(",");
+
+      } 
       
       
       
       /* initialize the formattedData array using rows and columns */
-      
-      
+      int j = unformattedData.size();
+      formattedData = new String[j][count];
+
       
       /* populate formattedData array with data from unformattedData */
+      for(int r = 0; r<formattedData.length; r++){
+        for(int c = 0; c<formattedData[0].length; c++){
+            if(c<formattedData.length-1){
+              int k = unformattedData.get(r).indexOf(",");
+              formattedData[r][c] = unformattedData.get(r).substring(0,k);
+              unformattedData.set(r,unformattedData.get(r).substring(k+1));
+            }
+            else{
+              formattedData[r][c] = unformattedData.get(r).substring(0);
+            }
+        }
+
+      } 
+      for(int r = 0; r<formattedData.length; r++){
+        for(int c = 0; c<formattedData[0].length; c++){
+            
+
+      } 
       
       
+
+
+
     } catch (FileNotFoundException e) {
       e.printStackTrace();
     }
