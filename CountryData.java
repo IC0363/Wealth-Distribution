b4357ebbe8ff757e409e1150281ce3bab8ce9d46
IC0363 <ichoi0221@gmail.com>
2024-05-23 13:28:25 -0400

    Update CountryData.java

--- CountryData.java
+++ CountryData.java
@@ -3,10 +3,44 @@ public class CountryData {
     private int year;
     private int population;
     private int[] incomeDeciles;
-    public CountryData(Data incomeData, String country, String year) {
+    public CountryData(StudentData incomeData, String country, String year) {
         name = country;
-	
-}
+        this.year = Integer.parseInt(year);
+        
+    }
+    public void setYear(int y){
+        year = y;
+    }
+    public int getYear(){
+        return year;
+    }
+    public void setName(String n){
+        name = n;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setPopulation(int p){
+        population = p;
+    }
+    public int getPopulation(){
+        return population;
+    }
+
+    public int totalIncome(){
+        int count = 0;
+        for(int i = 0; i<incomeDeciles.length; i++){
+            count+=incomeDeciles[i];
+        }
+        return count;
+    }
+
+    public double giniIndex(){
+        double count = 0;
+        for(int i)
+
+        
+    }
 
     
 }
