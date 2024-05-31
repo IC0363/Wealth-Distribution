ab2c53f6e2cf85764bb3469a19f98f9f3707b9d7
IC0363 <99269263+IC0363@users.noreply.github.com>
2024-05-30 22:11:49 -0400

    w

--- CountryData.java
+++ CountryData.java
@@ -1,76 +1,74 @@
 public class CountryData {
     private String name;
     private int year;
-    private int population;
     private int[] incomeDeciles;
-    
+
     public CountryData(StudentData incomeData, String country, String year) {
         this.name = country;
-        this.year = Integer.parseInt(year);
-        
-        int row = incomeData.getRow(country, year);
+        try {
+            this.year = Integer.parseInt(year);
+        } catch (NumberFormatException e) {
+            this.year = -1;
+        }
+
+        int row = incomeData.getRow(country, String.valueOf(this.year));
         if (row != -1) {
             String[] rowData = incomeData.getFormattedData()[row];
-            incomeDeciles = new int[rowData.length - 2];
-            for (int i = 2; i < rowData.length; i++) {
+            incomeDeciles = new int[10];
+            for (int i = 2; i < 12; i++) {
                 incomeDeciles[i - 2] = Integer.parseInt(rowData[i]);
             }
         } else {
-            incomeDeciles = new int[0]; 
+            incomeDeciles = new int[0];
         }
     }
-    
+
     public void setYear(int y) {
         year = y;
     }
-    
+
     public int getYear() {
         return year;
     }
-    
+
     public void setName(String n) {
         name = n;
     }
-    
+
     public String getName() {
         return name;
     }
-    
-    public void setPopulation(int p) {
-        population = p;
-    }
-    
-    public int getPopulation() {
-        return population;
-    }
-    
+
     public int[] getIncomeDeciles() {
         return incomeDeciles;
     }
-    
+
     public void setIncomeDeciles(int[] iD) {
         incomeDeciles = iD;
     }
-    
+
     public int totalIncome() {
-        if (incomeDeciles.length > 0) {
-            int count = 0;
-            for (int income : incomeDeciles) {
-                count += income;
-            }
-            return count;
+        int count = 0;
+        for (int income : incomeDeciles) {
+            count += income;
         }
-        return 0;
+        return count;
     }
 
     public double giniIndex() {
         if (incomeDeciles.length > 0) {
-            int g = incomeDeciles[0] + 2 * (incomeDeciles[0] + incomeDeciles[1]);
-            for (int i = 2; i < incomeDeciles.length - 1; i++) {
-                g += 2 * (g + incomeDeciles[i]);
+            int income = 0;
+            int sum = 0;
+            for (int i = 0; i < incomeDeciles.length; i++) {
+                income += incomeDeciles[i];
+                if (i == 0 || i == incomeDeciles.length - 1) {
+                    sum += income;
+                } 
+                else {
+                    sum += 2 * income;
+                }
             }
-            g += incomeDeciles[incomeDeciles.length - 1];
-            return (double) 0.1 / totalIncome() * g;
+            return 1 - ((double) 0.1 / totalIncome()) * sum;
         }
         return 0;
     }
